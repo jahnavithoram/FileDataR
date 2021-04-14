7e16e69e2456d724c020fcd039c6bc1978c73bad
Jahnavi Thoram <jahnavithoram999@gmail.com>
2021-04-15 00:16:10 +0530

    Add project files.

---
+++ FileDataRetrieve/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileDataRetrieve
+{
+    class Program
+    {
+        static void showdata(string fileName)
+        {
+            string[] arrLine = File.ReadAllLines(fileName);
+            foreach (var item in arrLine)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your File Name:\n");
+            string sourceFile = Console.ReadLine();
+           // var myfile = File.Create(sourceFile);
+         //   Console.WriteLine("CREATED...\n");
+           // myfile.Close();
+            if (!(File.Exists(sourceFile)))
+            {
+                Console.WriteLine("FILE DO NOT EXIST...\n");
+            }
+            else
+            {
+                Console.WriteLine("\nFILE DATA:\n");
+                showdata(sourceFile);
+            }
+        }
+    }
+}
